94563e47ecdc7a152b14522d7e6864a8666ec13b
AaronManuel15 <aaronmanuel15@gmail.com>
2022-08-09 14:04:11 -0700

    Function used to call correct monty functions

---
+++ getopcode.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+
+/**
+ * getop - Takes opcode and returns matching function call
+ * @opcode: opcode given from parsing line
+ *
+ * Return: function pointer
+ */
+
+void (*getop(char *opcode))(struct_t **stack, unsigned int linenumber)
+{
+	unsigned int i;
+
+	instruction_t opcodeFunction[] = {
+		{"push", push},
+		{"pull", pull},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
+		{NULL, NULL}
+	};
+
+	for (i = 0; opcodeFunction[i].opcode; i++)
+	{
+		if (strcmp(opcodeFunction[i].opcode, opcode) == 0)
+			break;
+	}
+	return (opcodeFunction[i].f)
+}
