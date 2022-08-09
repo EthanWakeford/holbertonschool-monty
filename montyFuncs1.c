c03c78ae1bbd10d33891d459248b07801c8cafe2
AaronManuel15 <aaronmanuel15@gmail.com>
2022-08-09 15:12:21 -0700

    monty function files. Currently empty. Added for compiling

---
+++ montyFuncs1.c
@@ -0,0 +1,40 @@
+#include "monty.h"
+
+/**
+ * push - creates a node in the linked list
+ * @stack: head of the linked list
+ * @linenumber: line number of the monty file
+ *
+ * Return: void
+ */
+
+void push(struct_t **stack, unsigned int linenumber)
+{
+
+}
+
+/**
+ * pall - prints all the values on the starting from the top of the stack
+ * @stack: head of the linked list
+ * @linenumber: line number of the monty file
+ *
+ * Return: void
+ */
+
+void pall(struct_t **stack, unsigned int linenumber)
+{
+
+}
+
+/**
+ * pint -  prints the value at the top of the stack, followed by a new line
+ * @stack: head of the linked list
+ * @linenumber: line number of the monty file
+ *
+ * Return: void
+ */
+
+void pint(struct_t **stack, unsigned int linenumber)
+{
+
+}
---
+++ montyFuncs2.c
@@ -0,0 +1,50 @@
+#include "monty.h"
+
+/**
+ * pop -  prints the value at the top of the stack, followed by a new line
+ * @stack: head of the linked list
+ * @linenumber: line number of the monty file
+ *
+ * Return: void
+ */
+void pop(struct_t **stack, unsigned int linenumber)
+{
+
+}
+
+/**
+ * swap -  swaps the first two nodes in stack
+ * @stack: head of the linked list
+ * @linenumber: line number of the monty file
+ *
+ * Return: void
+ */
+void swap(struct_t **stack, unsigned int linenumber)
+{
+
+}
+
+/**
+ * add - adds the first two nodes in stack
+ * @stack: head of the linked list
+ * @linenumber: line number of the monty file
+ *
+ * Return: void
+ */
+void add(struct_t **stack, unsigned int linenumber)
+{
+
+}
+
+/**
+ * nop - does nothing at all.. . . wow
+ * @stack: head of the linked list
+ * @linenumber: line number of the monty file
+ *
+ * Return: void
+ */
+void nop(struct_t **stack, unsigned int linenumber)
+{
+	(void *)stack;
+	(void *)linenumber;
+}
